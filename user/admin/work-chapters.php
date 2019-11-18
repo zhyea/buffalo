d895424c8e82c4b3b6a7a16b5d0cb93bc6531947
rui.zhang <rui.zhang@tendcloud.com>
2019-11-18 23:21:59 +0800

    尝试添加作品章节
    Change-Id: I87515385655c971ebbcb91d4aad3d37a37355064

--- user/admin/work-chapters.php
+++ user/admin/work-chapters.php
@@ -6,13 +6,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<div class="page-header">
 		<h3><span class="glyphicon glyphicon-book"></span> 天策府</h3>
-
 	</div>
 
-	<div class="row chapter-row">
-		<div class="col-md-4 col-xs-12 chapter-unit">章节一</div>
-		<div class="col-md-4 col-xs-12 chapter-unit">这里是章节二</div>
-		<div class="col-md-4 col-xs-12 chapter-unit">这里是章节3</div>
+	<div class="row chapter-container">
+		<div class="col-md-4 col-xs-12 chapter-unit"><div class="chapter"><a>章节一 </a><a><span class="glyphicon glyphicon-remove del"></span></a></div></div>
+		<div class="col-md-4 col-xs-12 chapter-unit"><div class="chapter"><a>章节一 </a><a><span class="glyphicon glyphicon-remove del"></span></a></div></div>
+		<div class="col-md-4 col-xs-12 chapter-unit"><div class="chapter"><a>章节一 </a><a><span class="glyphicon glyphicon-remove del"></span></a></div></div>
+		<div class="col-md-4 col-xs-12 chapter-unit"><div class="chapter"><a>章节一 </a><a><span class="glyphicon glyphicon-remove del"></span></a></div></div>
+		<div class="col-md-4 col-xs-12 chapter-unit"><div class="chapter"><a>章节一 </a><a><span class="glyphicon glyphicon-remove del"></span></a></div></div>
+		<div class="col-md-4 col-xs-12 chapter-unit"><div class="chapter"><a>章节一 </a><a><span class="glyphicon glyphicon-remove del"></span></a></div></div>
+		<div class="col-md-4 col-xs-12 chapter-unit"><div class="chapter"><a>章节一 </a><a><span class="glyphicon glyphicon-remove del"></span></a></div></div>
+		<div class="col-md-4 col-xs-12 chapter-unit"><div class="chapter"><a>章节一 </a><a><span class="glyphicon glyphicon-remove del"></span></a></div></div>
+		<div class="col-md-4 col-xs-12 chapter-unit"><div class="chapter"><a>章节一 </a><a><span class="glyphicon glyphicon-remove del"></span></a></div></div>
 	</div>
 
 
