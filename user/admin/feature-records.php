0b47e5574abdfed7171b39e9db4913f3d3dcb8cc
rui.zhang <rui.zhang@tendcloud.com>
2019-12-08 23:15:41 +0800

    尝试添加专题维护功能
    Change-Id: I0405b0fcf78abe34e7f76c77ca9e87d89146dda7

--- user/admin/feature-records.php
+++ user/admin/feature-records.php
@@ -7,21 +7,49 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="page-header">
 		<h3>专题 《<?= $name ?>》 - 列表</h3>
 	</div>
-	
+
+
+	<div class="modal fade" id="featureRecordSelect">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title">选择作品</h4>
+				</div>
+				<div class="modal-body">
+					<ul>
+						<li>输入关键字后执行选择</li>
+					</ul>
+					<div class="input-group">
+						<input type="text" class="form-control" id="featureRecordSelectInput" autocomplete="off">
+						<div class="input-group-btn">
+							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="">
+								<span class="caret"></span>
+							</button>
+							<ul class="dropdown-menu dropdown-menu-right" role="menu"></ul>
+						</div>
+						<!-- /btn-group -->
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default btn-primary-outline" data-dismiss="modal">关闭</button>
+				</div>
+			</div>
+			<!-- /.modal-content -->
+		</div>
+		<!-- /.modal-dialog -->
+	</div>
+	<!-- /.modal -->
 	
 	<div id="featureRecordTableToolbar">
-		<div class="input-group">
-			<input type="text" class="form-control" id="workSelector" required/>
-			<div class="input-group-btn">
-				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-					<span class="caret"></span>
-				</button>
-				<ul class="dropdown-menu dropdown-menu-right" role="menu">
-				</ul>
-				<a id="btnDelete" class="btn btn-default">
-					<i class="glyphicon glyphicon-minus"></i> 删除
-				</a>
-			</div>
+		<div class="btn-group">
+
+			<a class="btn btn-default" data-toggle="modal" data-target="#featureRecordSelect">
+				<i class="glyphicon glyphicon-plus"></i> 新增
+			</a>
+			<a id="btnDelete" class="btn btn-default">
+				<i class="glyphicon glyphicon-minus"></i> 删除
+			</a>
 		</div>
 	</div>
 	<table id="featureRecordTable"
@@ -49,7 +77,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <script charset="utf-8" src="<?= $ctx_admin ?>/static/js/jquery.min.js"></script>
 <script charset="utf-8" src="<?= $ctx_admin ?>/static/js/bootstrap-suggest.js"></script>
 <script>
-    $("#workSelector").bsSuggest({
+    $("#featureRecordSelectInput").bsSuggest({
         clearable: true,
         url: "<?= $ctx_site ?>/admin/category/data_all",
         showHeader: false,
