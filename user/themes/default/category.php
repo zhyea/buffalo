463b6a5a160b7f62f1bb515b278f21c0efd9c78c
rui.zhang <rui.zhang@tendcloud.com>
2019-12-18 23:15:35 +0800

    前端，添加分类页
    Change-Id: I1f46752c32a969c797ebf76d8eb43c0bd9e93650

--- user/themes/default/category.php
+++ user/themes/default/category.php
@@ -6,7 +6,7 @@ isset($ctx_theme) OR exit('No base url exists')
 
 <div class="container notice">
 	<ol class="breadcrumb">
-		<li><a href="#">首页</a></li>
+		<li><a href="#"><span class="glyphicon glyphicon-home"></span> 首页</a></li>
 		<li><a href="#">语文</a></li>
 		<li class="active">语文吃饭</li>
 	</ol>
@@ -92,7 +92,8 @@ isset($ctx_theme) OR exit('No base url exists')
 				<div class="title"><a href="#">望庐山瀑布</a></div>
 				<div class="author"><a href>李白</a></div>
 				<div class="intro">
-					日照香炉生紫烟，遥看瀑布挂前川，飞流直下三千只，疑是银河落九天。大江东去，浪淘尽，千古风流人物，遥想公瑾当年，小乔初嫁了，羽扇纶巾，雄姿英发，谈笑间樯橹灰飞烟灭. 山舞银蛇原驰蜡象欲与天公试比高，须晴日看红妆素裹分外妖娆。唐宗宋祖时候逊风骚秦皇汉武略输文采，一代天骄成吉思汗只识弯弓射大雕
+					日照香炉生紫烟，遥看瀑布挂前川，飞流直下三千只，疑是银河落九天。大江东去，浪淘尽，千古风流人物，遥想公瑾当年，小乔初嫁了，羽扇纶巾，雄姿英发，谈笑间樯橹灰飞烟灭.
+					山舞银蛇原驰蜡象欲与天公试比高，须晴日看红妆素裹分外妖娆。唐宗宋祖时候逊风骚秦皇汉武略输文采，一代天骄成吉思汗只识弯弓射大雕
 				</div>
 			</div>
 		</div>
@@ -108,7 +109,12 @@ isset($ctx_theme) OR exit('No base url exists')
 				</div>
 			</div>
 		</div>
-
 	</div>
 
+	<div class="pagination">
+		<a href="#">1</a>
+		<a href="#">2</a>
+		<a href="#">3</a>
+		<a href="#">4</a>
+	</div>
 </div>
