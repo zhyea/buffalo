21e34d2e778db4a6b377435868182181dc86d3d8
rui.zhang <rui.zhang@tendcloud.com>
2019-12-21 23:18:01 +0800

    文章页前端实现
    Change-Id: Iae4d7cce16e325e750771b4bc9b2e537fefc4572

--- user/themes/default/chapter.php
+++ user/themes/default/chapter.php
@@ -4,6 +4,8 @@ isset($ctx_theme) OR exit('No base url exists')
 ?>
 
 
+<script charset="utf-8" src="<?= $ctx_theme ?>/static/js/reader.js" type="text/javascript"></script>
+
 <div class="container notice">
 	<ol class="breadcrumb">
 		<li><a href="#"><span class="glyphicon glyphicon-home"></span> 首页</a></li>
@@ -13,60 +15,38 @@ isset($ctx_theme) OR exit('No base url exists')
 </div>
 
 <div class="main">
-	<div class="work-header">
-		<span class="title">梦游天姥吟留别</span>
-		<span class="author">作者：<a href="#">李白</a></span>
+	<div class="row readerTools">
+		<script type="text/javascript">
+            if (system.win || system.mac || system.xll) {
+                readerSet();
+            }
+		</script>
 	</div>
 
-	<div class="work-neck row">
-		<div class="col-md-2 cover">
-			<img src="<?php echo $ctx_theme ?>/static/imgs/noimg.jpg" width="120px" height="172px"/>
+	<div class="row" id="contentContainer">
+
+		<div class="row chapter-name">
+			第一章 把酒问青天
 		</div>
 
-		<div class="col-md-10 col-xs-12 brief">
-			<div class="intro">
-				酷爱蒸汽机械的高冷男西泽尔，总是孤独寂寞的与世界相处，心里的小小梦想是希望自己和妹妹平静而幸福的生活。这样的一个人，有人觉得他是希望，有人觉得他是魔鬼，他所到之处，就有腥风血雨跟随，从锡兰到马斯顿，他把灾难从一座城市带往另一座城市……为了保护自己所爱的人，让妹妹可以无忧无虑成长，面对敌人的冰寒之剑，西泽尔将作何选择？命运又将何去何从？！
-			</div>
-			<div class="relate"><i class="glyphicon glyphicon-tags"></i> 相关作品： <a href="#">以待天倾</a><a href="#">赋得古原草送别</a><a href="#">念奴娇·赤壁怀古</a></div>
+		<div class="row chapter-nav">
+			<a>上一章</a> ← <a>返回目录</a> → <a>下一章</a>
+		</div>
+
+		<div class="row chapter-content" id="contentText" style="">
+			zzzzzzzzzzzzzzzzzzz
+			zzzzzzzzzzzzzzzzz
+			zzzzzzzzzz
+		</div>
+
+		<div class="row chapter-nav">
+			<a>上一章</a> ← <a>返回目录</a> → <a>没有了</a>
 		</div>
 	</div>
 
-	<div class="work-body">
-		<div class="row">
-			<div class="col-md-12 col-xs-12 volume"><i class="glyphicon glyphicon-bookmark"></i> 第一卷</div>
-			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
-			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
-			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
-			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
-			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
-			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
-			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
-			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
-			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
-		</div>
-		<div class="row">
-			<div class="col-md-12 col-xs-12 volume"><i class="glyphicon glyphicon-bookmark"></i> 第一卷</div>
-			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
-			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
-			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
-			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
-			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
-			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
-			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
-			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
-			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
-		</div>
-		<div class="row">
-			<div class="col-md-12 col-xs-12 volume"><i class="glyphicon glyphicon-bookmark"></i> 第一卷</div>
-			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
-			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
-			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
-			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
-			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
-			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
-			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
-			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
-			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
-		</div>
-	</div>
 </div>
+
+
+<script type="text/javascript">
+    window.addEventListener('load', LoadReadSet, false);
+</script>
---
+++ user/themes/default/work.php
@@ -0,0 +1,72 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+isset($ctx_theme) OR exit('No base url exists')
+?>
+
+
+<div class="container notice">
+	<ol class="breadcrumb">
+		<li><a href="#"><span class="glyphicon glyphicon-home"></span> 首页</a></li>
+		<li><a href="#">语文</a></li>
+		<li class="active">梦游天姥吟留别</li>
+	</ol>
+</div>
+
+<div class="main">
+	<div class="work-header">
+		<span class="title">梦游天姥吟留别</span>
+		<span class="author">作者：<a href="#">李白</a></span>
+	</div>
+
+	<div class="work-neck row">
+		<div class="col-md-2 cover">
+			<img src="<?php echo $ctx_theme ?>/static/imgs/noimg.jpg" width="120px" height="172px"/>
+		</div>
+
+		<div class="col-md-10 col-xs-12 brief">
+			<div class="intro">
+				酷爱蒸汽机械的高冷男西泽尔，总是孤独寂寞的与世界相处，心里的小小梦想是希望自己和妹妹平静而幸福的生活。这样的一个人，有人觉得他是希望，有人觉得他是魔鬼，他所到之处，就有腥风血雨跟随，从锡兰到马斯顿，他把灾难从一座城市带往另一座城市……为了保护自己所爱的人，让妹妹可以无忧无虑成长，面对敌人的冰寒之剑，西泽尔将作何选择？命运又将何去何从？！
+			</div>
+			<div class="relate"><i class="glyphicon glyphicon-tags"></i> 相关作品： <a href="#">以待天倾</a><a href="#">赋得古原草送别</a><a href="#">念奴娇·赤壁怀古</a></div>
+		</div>
+	</div>
+
+	<div class="work-body">
+		<div class="row">
+			<div class="col-md-12 col-xs-12 volume"><i class="glyphicon glyphicon-bookmark"></i> 第一卷</div>
+			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
+			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
+			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
+			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
+			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
+			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
+			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
+			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
+			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 col-xs-12 volume"><i class="glyphicon glyphicon-bookmark"></i> 第一卷</div>
+			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
+			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
+			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
+			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
+			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
+			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
+			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
+			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
+			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 col-xs-12 volume"><i class="glyphicon glyphicon-bookmark"></i> 第一卷</div>
+			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
+			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
+			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
+			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
+			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
+			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
+			<div class="col-md-4 col-xs-12 chapter">翡冷翠</div>
+			<div class="col-md-4 col-xs-12 chapter">佛罗伦萨之夜</div>
+			<div class="col-md-4 col-xs-12 chapter">若马大道何其多</div>
+		</div>
+	</div>
+</div>
